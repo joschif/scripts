32a4bb02b89487190e429760737f64074e0528bd
astair <jonas.simon.fleck@gmail.com>
2017-11-16 17:13:21 +0100

    Changed sam/bam_cluster.py again for

--- bam_cluster.py
+++ bam_cluster.py
@@ -7,7 +7,7 @@ import pysam
 
 # FUNC
 def interface():
-    parser = argparse.ArgumentParser(description='Takes one BAM file and one FASTA file with contigs or a list of contig IDs. Exrtacts the reads from the fasta file that align to the given contigs and writes tehm to a gzipped multi-FASTA.')
+    parser = argparse.ArgumentParser(description='Takes one BAM file and one FASTA file with contigs or a list of contig IDs. Exrtacts the reads from the fasta file that align to the given contigs and writes them to a gzipped multi-FASTA.')
 
     parser.add_argument('BAM',
                         type=str,
@@ -19,7 +19,8 @@ def interface():
                         metavar='<CONTIGS>',
                         help='Either FASTA file with contigs or list of contig IDs.') 
 
-    parser.add_argument('OUT',
+    parser.add_argument('-o', '--output-file',
+                        dest='out',
                         type=str,
                         metavar='<OUT_FILE>',
                         help='File to output the clustered reads as multi-FASTA.')
@@ -34,25 +35,28 @@ if __name__ == "__main__":
 
     bam_file = pysam.AlignmentFile(args.BAM, 'rb')
     contigs = args.CONTIGS
-    out_file = args.OUT
+    if args.out:
+        out_file = args.out
+    else:
+        out_file = contigs.split(".")[0] + ".fq.gz"
 
     # All IDs in cluster to set
-    contig_ids = set()
+    contig_ids = []
     with open(contigs, 'r') as f:
         line = f.readline()
-        contig_ids.add(line)
+        contig_ids.append(line)
         if line.startswith('>'):
             for line in f:
                 if line.startswith('>'):
-                    contig_ids.add(line)
+                    contig_ids.append(line)
         else:
             for line in f:
-                contig_ids.add(line)
+                contig_ids.append(line)
 
     # Extract from BAM file
     with gzip.open(out_file, 'wt') as f:
-        for read in bam_file.fetch():
-            if read.reference_name in contig_ids:
+        for ID in contig_ids:
+            for read in bam_file.fetch(ID):
                 f.write(read.query_sequence + '\n')
                 f.write(read.query_name + '\n')
 
