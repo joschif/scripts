6162e4215dbb92294b4bb30b250c9d9d2ea59f86
astair <jonas.simon.fleck@gmail.com>
2017-11-24 15:11:06 +0100

    check_reads.py now with itertools.combinations()

--- check_reads.py
+++ check_reads.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+import sys
 import argparse
 import gzip
 import glob
 import os
+import itertools
 
 # FUNC
 def interface():
@@ -30,29 +32,65 @@ def open_gz(infile, mode='rt'):
 if __name__ == "__main__":
     args = interface()
     reads_dir = os.path.abspath(args.READS_DIR)
-    files = glob.glob(reads_dir + '/*')
-    base = [name.split('.')[0] for name in files]
-    ext = ['.'.join(name.split('.')[1:]) for name in files]
-    lines = []
+    names = glob.glob(reads_dir + '/*')
+    names.sort()
+    base = [name.split('.')[0] for name in names]
+    ext = ['.'.join(name.split('.')[1:]) for name in names]
+    files = [list(rec) for rec in zip(names, base, ext)]
 
     for file in files:
-        with open_gz(file) as f:
-            lines.append(f.readline())
+        with open_gz(file[0]) as f:
+            line1 = f.readline().strip()
+            if line1.startswith('>'):
+                file.append(line1)
+                for line in f:
+                    if line.startswith('>'):
+                        file.append(line.strip())
+                        break     
+            if line1.startswith('@'):
+                file.append(line1)
+                [f.readline() for n in range(3)]
+                file.append(f.readline().strip())
+            else:
+                raise TypeError("Files have to be in FASTQ or FASTA format")
 
-    for n in range(len(lines)):
-        for m in range(len(lines)):
-            if (lines[n] != lines[m]) and (lines[n][:-1] == lines[m][:-1]):
-                first = files.pop(n)
-                base1 = base.pop(n)
-                ext1 = ext.pop(n)
-                os.rename(first, base1 + '.pair.1.' + ext1)                 
-                second = files.pop(m)
-                base2 = base.pop(m)
-                ext2 = ext.pop(m)
-                os.rename(first, base2 + '.pair.2.' + ext2) 
 
-    for n in range(len(files)):
-        os.rename(files[n], base[n] + '.single.' + ext[n])
+    inter = []
+    for file in files: 
+        if file[3][:-1] == file[4][:-1]:
+            inter.append(file)
+            files.remove(file)
+
+    firsts = []
+    seconds = []
+    for comb in itertools.combinations(files, 2):
+        if comb[0][3][:-1] == comb[1][3][:-1] and comb[0][3] != comb[1][3]:
+            firsts.append(comb[0])
+            seconds.append(comb[1])
+
+    singles = []
+    for file in files:
+        if file not in firsts and file not in seconds:
+            singles.append(file)
+
+    for rec in inter:
+        if '.pairs.interleaved.' not in rec[2]:
+            os.rename(rec[0], rec[1] + '.pairs.interleaved.' + rec[2])                 
+    
+    for rec in firsts:
+        if '.pair.1.' not in rec[2] and '.pair.2.' not in rec[2]:
+            os.rename(rec[0], rec[1] + '.pair.1.' + rec[2])      
+
+    for rec in seconds:
+        if '.pair.1.' not in rec[2] and '.pair.2.' not in rec[2]:
+            os.rename(rec[0], rec[1] + '.pair.2.' + rec[2])                 
+    
+    for rec in singles:
+        if '.single.' not in rec[2]:
+            os.rename(rec[0], rec[1] + '.single.' + rec[2])                 
+    
+
+    
 
 
 
